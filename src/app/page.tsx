fa06f07767f0d9b1845310246b5dac99d5e1cac1
qowldud <bjy6631@daum.net>
2026-07-14 14:58:02 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <h1 className="text-2xl font-semibold">Tudack Frontend</h1>
+    </div>
+  );
+}
